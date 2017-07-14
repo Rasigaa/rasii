fa2d9f79858cbcb8f8486d0cac108f7dd7971a9f
Rasigaa <ranirasi45@gmail.com>
2017-07-14 14:46:43 +0530

    Create Mulstri.java

---
+++ Mulstri.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Mulstri{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+String b=s.nextLine();
+long c=(Long.parseLong(a))*(Long.parseLong(b));
+String d=new String();
+d=String.valueOf(c);
+System.out.print(d);
+}}

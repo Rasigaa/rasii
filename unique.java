b630021f2b925d23cd09f0d1ba67432516ea7507
Rasigaa <ranirasi45@gmail.com>
2017-09-02 14:32:21 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class unique{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+String b[]=a.split(" ");
+int i,k=0,m=0;
+int c[]=new int[b.length];
+for(i=0;i<b.length;i++)
+{
+c[i]=Integer.parseInt(b[i]);
+}
+Arrays.sort(c);
+for(i=0;i<c.length-1;i++)
+{
+if(c[i]==c[i+1])
+{
+i++;
+k=k+2;
+}
+else
+{
+System.out.print(c[i]);
+m++;
+}}
+if(k==c.length-1&&m==0)
+System.out.print(c[c.length-1]);
+}}

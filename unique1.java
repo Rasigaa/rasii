32ebd3f6e4bfa9a5bc1d5e60bfeea06d631cb017
Rasigaa <ranirasi45@gmail.com>
2017-09-02 14:29:21 +0530

    Update and rename unique.java to unique1.java

--- unique1.java
+++ unique1.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class unique{
+public class unique1{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
 String a=s.nextLine();

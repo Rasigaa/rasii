739402122b12e1b62ac62320fcf99219eda1264f
Rasigaa <ranirasi45@gmail.com>
2017-09-04 00:20:15 +0530

    Create rw.java

---
+++ rw.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class rw{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+String b[]=a.split(" ");
+System.out.print(b[1]+" "+b[0]);
+}}

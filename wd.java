0c626a4b94513e06c220b498a280d241ee6363ad
Rasigaa <ranirasi45@gmail.com>
2017-09-04 21:44:21 +0530

    Create wd.java

---
+++ wd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class wd{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+String b=a.toLowerCase();
+String c="sunday";
+if(c.equals(a))
+System.out.print("false");
+else
+System.out.print("true");
+}}
